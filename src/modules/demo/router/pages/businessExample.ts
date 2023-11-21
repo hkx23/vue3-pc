8f515f2280cab11b6df9d77b8aa191013448789b
Jesse <jcay60@163.com>
2023-11-21 17:12:41 +0800

    fix: 修改Router为相对路径

--- src/modules/demo/router/pages/businessExample.ts
+++ src/modules/demo/router/pages/businessExample.ts
@@ -2,7 +2,7 @@ export default [
   {
     path: '/businessExample',
     name: 'businessExample',
-    component: () => import('@/modules/demo/pages/componentExample/index.vue'),
+    component: () => import('../../pages/componentExample/index.vue'),
     meta: {
       title: {
         zh_CN: '业务选择组件例子',
--- src/modules/demo/router/pages/user.ts
+++ src/modules/demo/router/pages/user.ts
@@ -2,7 +2,7 @@ export default [
   {
     path: '/user',
     name: 'user',
-    component: () => import('@/modules/system/pages/user/index.vue'),
+    component: () => import('../../pages/user/index.vue'),
     meta: {
       title: {
         zh_CN: '用户',
