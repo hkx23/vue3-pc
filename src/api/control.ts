945124cf3d67cea3dcd243bc5a79a0f01456ff54
‘小新’ <2403805044@qq.com>
2023-11-24 17:58:01 +0800

    feat: 工作中心页面基本跳转功能渲染

--- src/api/control.ts
+++ src/api/control.ts
@@ -9,6 +9,106 @@
  * ---------------------------------------------------------------
  */
 
+/** 筛选字段 */
+export interface Filter {
+  field?: string;
+  operator?: 'EQ' | 'GT' | 'LT' | 'LTE' | 'GTE' | 'LIKE';
+  value?: string;
+}
+
+/** 排序字段 */
+export interface SortParam {
+  sortBy?: string;
+  descending?: boolean;
+}
+
+export interface WorkcenterSearch {
+  /**
+   * 页码
+   * @format int32
+   */
+  pageNum?: number;
+  /**
+   * 页最大记录条数
+   * @format int32
+   */
+  pageSize?: number;
+  /**
+   * id
+   * @format int32
+   */
+  id?: number;
+  /** 多个id */
+  ids?: number[];
+  /** 工作中心-类别 */
+  category?: number[];
+  /** 工作中心编码名称模糊查询 */
+  workcenterword?: string;
+  workshopID?: string;
+  /** 工作中心-状态 */
+  state?: number[];
+  /** 排序字段 */
+  sorts?: SortParam[];
+  /** 筛选字段 */
+  filters?: Filter[];
+}
+
+/** 响应数据 */
+export type PagingDataWorkstationAuthVO = {
+  list?: WorkstationAuthVO[];
+  /** @format int32 */
+  total?: number;
+} | null;
+
+/** 通用响应类 */
+export interface ResultPagingDataWorkstationAuthVO {
+  /**
+   * 响应代码
+   * @format int32
+   */
+  code?: number;
+  /** 提示信息 */
+  message?: string;
+  /** 响应数据 */
+  data?: PagingDataWorkstationAuthVO;
+}
+
+/** 工站权限显示 */
+export interface WorkstationAuthVO {
+  id?: string;
+  eid?: string;
+  oid?: string;
+  /** 用户名 */
+  userName?: string;
+  /** 姓名 */
+  personName?: string;
+  /** 工站编码 */
+  workstationCode?: string;
+  /** 工站名称 */
+  workstationName?: string;
+  /** 工站描述 */
+  workstationDesc?: string;
+  /** 工作中心名称 */
+  workcenterName?: string;
+  /** 工序名称 */
+  processName?: string;
+  /**
+   * 状态
+   * @format int32
+   */
+  state?: number;
+  /** 创建人 */
+  creator?: string;
+  /** 创建时间 */
+  timeCreate?: string;
+  /** 修改人 */
+  modifier?: string;
+  /** 修改时间 */
+  timeModified?: string;
+  pworkcenterId?: string;
+  pprocessId?: string;
+}
+
 export interface CommonSearch {
   /** @format int32 */
   pageNum?: number;
@@ -21,17 +121,6 @@ export interface CommonSearch {
   category?: string;
   sorts?: SortParam[];
   filters?: Filter[];
-}
-
-export interface Filter {
-  field?: string;
-  operator?: 'EQ' | 'GT' | 'LT' | 'LTE' | 'GTE' | 'LIKE';
-  value?: string;
-}
-
-export interface SortParam {
-  sortBy?: string;
-  descending?: boolean;
 }
 
 /** 响应数据 */
@@ -102,6 +191,64 @@ export interface ResultWorkstation {
   data?: Workstation;
 }
 
+/** 通用响应类 */
+export interface ResultObject {
+  /**
+   * 响应代码
+   * @format int32
+   */
+  code?: number;
+  /** 提示信息 */
+  message?: string;
+  /** 响应数据 */
+  data?: object | null;
+}
+
+export interface WorkstationSearch {
+  /**
+   * 页码
+   * @format int32
+   */
+  pageNum?: number;
+  /**
+   * 页最大记录条数
+   * @format int32
+   */
+  pageSize?: number;
+  /** 工站编码名称模糊 */
+  workstaion?: string;
+  /** 工站-状态 */
+  state?: number[];
+  /** 工作中心编码名称模糊 */
+  workcenter?: string;
+  /** 工序编码名称模糊 */
+  process?: string;
+  /** 排序字段 */
+  sorts?: SortParam[];
+  /** 筛选字段 */
+  filters?: Filter[];
+}
+
+/** 响应数据 */
+export type PagingDataWorkstationVO = {
+  list?: WorkstationVO[];
+  /** @format int32 */
+  total?: number;
+} | null;
+
+/** 通用响应类 */
+export interface ResultPagingDataWorkstationVO {
+  /**
+   * 响应代码
+   * @format int32
+   */
+  code?: number;
+  /** 提示信息 */
+  message?: string;
+  /** 响应数据 */
+  data?: PagingDataWorkstationVO;
+}
+
 /** 显示工站 */
 export interface WorkstationVO {
   id?: string;
@@ -132,72 +279,6 @@ export interface WorkstationVO {
   timeModified?: string;
   pworkcenterId?: string;
   pprocessId?: string;
-}
-
-/** 通用响应类 */
-export interface ResultObject {
-  /**
-   * 响应代码
-   * @format int32
-   */
-  code?: number;
-  /** 提示信息 */
-  message?: string;
-  /** 响应数据 */
-  data?: object | null;
-}
-
-export interface WorkstationSearch {
-  /** @format int32 */
-  pageNum?: number;
-  /** @format int32 */
-  pageSize?: number;
-  workstaion?: string;
-  state?: number[];
-  workcenter?: string;
-  process?: string;
-  sorts?: SortParam[];
-  filters?: Filter[];
-}
-
-/** 响应数据 */
-export type PagingDataWorkstationVO = {
-  list?: WorkstationVO[];
-  /** @format int32 */
-  total?: number;
-} | null;
-
-/** 通用响应类 */
-export interface ResultPagingDataWorkstationVO {
-  /**
-   * 响应代码
-   * @format int32
-   */
-  code?: number;
-  /** 提示信息 */
-  message?: string;
-  /** 响应数据 */
-  data?: PagingDataWorkstationVO;
-}
-
-/** 响应数据 */
-export type PagingDataWorkcenter = {
-  list?: Workcenter[];
-  /** @format int32 */
-  total?: number;
-} | null;
-
-/** 通用响应类 */
-export interface ResultPagingDataWorkcenter {
-  /**
-   * 响应代码
-   * @format int32
-   */
-  code?: number;
-  /** 提示信息 */
-  message?: string;
-  /** 响应数据 */
-  data?: PagingDataWorkcenter;
 }
 
 /** 工作中心 */
@@ -249,6 +330,26 @@ export interface Workcenter {
   ownerId?: string;
 }
 
+/** 响应数据 */
+export type PagingDataWorkcenter = {
+  list?: Workcenter[];
+  /** @format int32 */
+  total?: number;
+} | null;
+
+/** 通用响应类 */
+export interface ResultPagingDataWorkcenter {
+  /**
+   * 响应代码
+   * @format int32
+   */
+  code?: number;
+  /** 提示信息 */
+  message?: string;
+  /** 响应数据 */
+  data?: PagingDataWorkcenter;
+}
+
 /** 通用响应类 */
 export interface ResultWorkcenter {
   /**
@@ -260,6 +361,89 @@ export interface ResultWorkcenter {
   message?: string;
   /** 工作中心 */
   data?: Workcenter;
+}
+
+/** 响应数据 */
+export type PagingDataWorkcenterVO = {
+  list?: WorkcenterVO[];
+  /** @format int32 */
+  total?: number;
+} | null;
+
+/** 通用响应类 */
+export interface ResultPagingDataWorkcenterVO {
+  /**
+   * 响应代码
+   * @format int32
+   */
+  code?: number;
+  /** 提示信息 */
+  message?: string;
+  /** 响应数据 */
+  data?: PagingDataWorkcenterVO;
+}
+
+/** 工作中心显示 */
+export interface WorkcenterVO {
+  id?: string;
+  eid?: string;
+  oid?: string;
+  /** 工作中心编号 */
+  wcCode?: string;
+  /** 工作中心名称 */
+  wcName?: string;
+  /**
+   * 工作中心类型
+   * @format int32
+   */
+  wcObjectType?: number;
+  /** 工作中心类型 */
+  wcType?: string;
+  workshopID?: string;
+  /** 所属车间编码 */
+  workshopCode?: string;
+  /** 所属车间名称 */
+  workshopName?: string;
+  /** 工作中心地点 */
+  wcLocation?: string;
+  parentWcId?: string;
+  /** 父工作中心编码 */
+  parentWcCode?: string;
+  /** 负责人名称 */
+  owner?: string;
+  wcObjectId?: string;
+  /** 关联设备编码 */
+  wcObjectCode?: string;
+  /**
+   * 顺序号
+   * @format int32
+   */
+  wcSeq?: number;
+  /**
+   * 状态
+   * @format int32
+   */
+  state?: number;
+  /**
+   * 工作中心-工作区总记录数
+   * @format int32
+   */
+  area?: number;
+  /**
+   * 工作中心-生产线总记录数
+   * @format int32
+   */
+  line?: number;
+  /**
+   * 工作中心-工段总记录数
+   * @format int32
+   */
+  section?: number;
+  /**
+   * 工作中心-设备总记录数
+   * @format int32
+   */
+  device?: number;
 }
 
 /** 响应数据 */
@@ -326,6 +510,20 @@ export interface ResultProcess {
   message?: string;
   /** 工序 */
   data?: Process;
+}
+
+export interface ProcessVO {
+  /** 工序代码 */
+  processCode?: string;
+  /** 工序名称 */
+  processName?: string;
+  /** 工序描述 */
+  processDesc?: string;
+  /**
+   * 工序状态
+   * @format int32
+   */
+  state?: number;
 }
 
 /** 工单排产表 */
@@ -594,6 +792,19 @@ export interface ResultMo {
   data?: Mo;
 }
 
+/** 通用响应类 */
+export interface ResultWorkcenterVO {
+  /**
+   * 响应代码
+   * @format int32
+   */
+  code?: number;
+  /** 提示信息 */
+  message?: string;
+  /** 工作中心显示 */
+  data?: WorkcenterVO;
+}
+
 /**
  * @title scm项目
  * @version v1
@@ -602,6 +813,22 @@ export interface ResultMo {
  */
 
 export const api = {
+  workstationAuth: {
+    /**
+     * No description
+     *
+     * @tags 工站权限
+     * @name Getlist
+     * @summary 查询工作中心
+     * @request POST:/workstationAuth/getList
+     * @secure
+     */
+    getlist: (data: WorkcenterSearch) =>
+      http.request<ResultPagingDataWorkstationAuthVO['data']>(`/api/control/workstationAuth/getList`, {
+        method: 'POST',
+        body: data as any,
+      }),
+  },
   workstation: {
     /**
      * No description
@@ -639,7 +866,7 @@ export const api = {
      * @request POST:/workstation/items/modify
      * @secure
      */
-    edit: (data: WorkstationVO) =>
+    edit: (data: Workstation) =>
       http.request<ResultObject['data']>(`/api/control/workstation/items/modify`, {
         method: 'POST',
         body: data as any,
@@ -654,7 +881,7 @@ export const api = {
      * @request POST:/workstation/items/add
      * @secure
      */
-    add: (data: WorkstationVO) =>
+    add: (data: Workstation) =>
       http.request<ResultObject['data']>(`/api/control/workstation/items/add`, {
         method: 'POST',
         body: data as any,
@@ -680,8 +907,37 @@ export const api = {
      * No description
      *
      * @tags 工作中心
+     * @name Remove
+     * @summary 删除工作中心
+     * @request POST:/workcenter/remove
+     * @secure
+     */
+    remove: (data: WorkcenterSearch) =>
+      http.request<ResultObject['data']>(`/api/control/workcenter/remove`, {
+        method: 'POST',
+        body: data as any,
+      }),
+
+    /**
+     * No description
+     *
+     * @tags 工作中心
+     * @name Modify
+     * @summary 编辑工作中心
+     * @request POST:/workcenter/modify
+     * @secure
+     */
+    modify: (data: Workcenter) =>
+      http.request<ResultObject['data']>(`/api/control/workcenter/modify`, {
+        method: 'POST',
+        body: data as any,
+      }),
+
+    /**
+     * No description
+     *
+     * @tags 工作中心
      * @name Search
-     * @summary 工作中心查询
      * @request POST:/workcenter/items
      * @secure
      */
@@ -702,6 +958,79 @@ export const api = {
     getItemById: (id: string) =>
       http.request<ResultWorkcenter['data']>(`/api/control/workcenter/items/${id}`, {
         method: 'POST',
+      }),
+
+    /**
+     * No description
+     *
+     * @tags 工作中心
+     * @name Getlist
+     * @summary 查询工作中心
+     * @request POST:/workcenter/getList
+     * @secure
+     */
+    getlist: (data: WorkcenterSearch) =>
+      http.request<ResultPagingDataWorkcenterVO['data']>(`/api/control/workcenter/getList`, {
+        method: 'POST',
+        body: data as any,
+      }),
+
+    /**
+     * No description
+     *
+     * @tags 工作中心
+     * @name GetChildCenter
+     * @summary 查询子工作中心
+     * @request POST:/workcenter/getChildCenter
+     * @secure
+     */
+    getChildCenter: (data: WorkcenterSearch) =>
+      http.request<ResultPagingDataWorkcenterVO['data']>(`/api/control/workcenter/getChildCenter`, {
+        method: 'POST',
+        body: data as any,
+      }),
+
+    /**
+     * No description
+     *
+     * @tags 工作中心
+     * @name Add
+     * @summary 新增工作中心
+     * @request POST:/workcenter/add
+     * @secure
+     */
+    add: (data: Workcenter) =>
+      http.request<ResultObject['data']>(`/api/control/workcenter/add`, {
+        method: 'POST',
+        body: data as any,
+      }),
+
+    /**
+     * No description
+     *
+     * @tags 工作中心
+     * @name GetTagCount
+     * @summary 工作中心获取tag总记录数
+     * @request GET:/workcenter/getTagCount
+     * @secure
+     */
+    getTagCount: () =>
+      http.request<ResultWorkcenterVO['data']>(`/api/control/workcenter/getTagCount`, {
+        method: 'GET',
+      }),
+
+    /**
+     * No description
+     *
+     * @tags 工作中心
+     * @name GetCategory
+     * @summary 工作中心获取组织层级
+     * @request GET:/workcenter/getCategory
+     * @secure
+     */
+    getCategory: () =>
+      http.request<ResultObject['data']>(`/api/control/workcenter/getCategory`, {
+        method: 'GET',
       }),
   },
   process: {
@@ -730,6 +1059,36 @@ export const api = {
     getItemById: (id: string) =>
       http.request<ResultProcess['data']>(`/api/control/process/items/${id}`, {
         method: 'POST',
+      }),
+
+    /**
+     * No description
+     *
+     * @tags 工序
+     * @name EditById
+     * @summary 工序编辑
+     * @request POST:/process/editById
+     * @secure
+     */
+    editById: (data: ProcessVO) =>
+      http.request<ResultObject['data']>(`/api/control/process/editById`, {
+        method: 'POST',
+        body: data as any,
+      }),
+
+    /**
+     * No description
+     *
+     * @tags 工序
+     * @name AddById
+     * @summary 工序新增
+     * @request POST:/process/addById
+     * @secure
+     */
+    addById: (data: ProcessVO) =>
+      http.request<ResultObject['data']>(`/api/control/process/addById`, {
+        method: 'POST',
+        body: data as any,
       }),
   },
   moSchedule: {
