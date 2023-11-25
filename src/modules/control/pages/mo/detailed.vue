c89b6cd9a8d09f5417ba0bf634873de9a4435b27
Ben <6489@qq.com>
2023-11-25 17:53:04 +0800

    feat: 新增工单管理修改路线的功能

--- src/modules/control/pages/mo/detailed.vue
+++ src/modules/control/pages/mo/detailed.vue
@@ -1,46 +1,52 @@
 <template>
   <div class="detailed-box">
     <!-- from -->
-    <t-row>
-      <t-col :span="12">
-        <span> {{ row.moCode }}</span> <span> {{ row.moClass }}</span> <span> {{ row.status }}</span>
-      </t-col>
-    </t-row>
-    <t-form layout="inline" :data="formData" :show-cancel="true" :show-error-message="false" @submit="submit">
-      <t-card :bordered="false">
-        <header class="form-item-box">
-          <t-form-item label="生产车间">
-            {{ row.workshopName }}
-          </t-form-item>
-          <t-form-item label="工作中心"> {{ row.workCenterName }}</t-form-item>
-          <t-form-item label="产品编码"> {{ row.mitemCode }}</t-form-item>
-        </header>
-        <div class="form-item-box">
-          <t-form-item label="产品名称"> {{ row.mitemName }}</t-form-item>
-          <t-form-item label="计划数量">
-            {{ row.planQty }}
-          </t-form-item>
-          <t-form-item label="完工数量"> {{ row.completedQty }}</t-form-item>
-        </div>
-        <div class="form-item-box">
-          <t-form-item label="计划开始时间"> {{ row.datetimePlanStart }}</t-form-item>
-          <t-form-item label="计划完成时间"> {{ row.datetimePlanEnd }}</t-form-item>
-          <t-form-item label="入库仓库"> {{ row.warehouse_code }}</t-form-item>
-        </div>
-        <div class="form-item-box">
-          <t-form-item label="销售订单"> {{ row.so_no }}</t-form-item>
-          <t-form-item label="工艺路线版本"> {{ row.routingName }}</t-form-item>
-        </div>
-        <div class="form-item-box">
-          <t-form-item label="备注"> {{ row.memo }}</t-form-item>
-        </div>
-      </t-card>
-    </t-form>
+    <t-card class="list-card-container" :bordered="true">
+      <t-row>
+        <t-col :span="4">
+          <t-tag shape="round" size="large">{{ row.moCode }}</t-tag>
+        </t-col>
+        <t-col :span="4">
+          <t-tag shape="round" theme="primary" size="large">{{ row.moClassName }}</t-tag>
+        </t-col>
+        <t-col :span="4">
+          <t-tag shape="round" size="large">{{ row.statusName }}</t-tag>
+        </t-col>
+      </t-row>
+    </t-card>
+    <t-card :bordered="false">
+      <div class="form-item-box">
+        <t-form-item label="生产车间">
+          {{ row.workshopName }}
+        </t-form-item>
+        <t-form-item label="工作中心"> {{ row.workCenterName }}</t-form-item>
+        <t-form-item label="产品编码"> {{ row.mitemCode }}</t-form-item>
+      </div>
+      <div class="form-item-box">
+        <t-form-item label="产品名称"> {{ row.mitemName }}</t-form-item>
+        <t-form-item label="计划数量">
+          {{ row.planQty }}
+        </t-form-item>
+        <t-form-item label="完工数量"> {{ row.completedQty }}</t-form-item>
+      </div>
+      <div class="form-item-box">
+        <t-form-item label="计划开始时间"> {{ row.datetimePlanStart }}</t-form-item>
+        <t-form-item label="计划完成时间"> {{ row.datetimePlanEnd }}</t-form-item>
+        <t-form-item label="入库仓库"> {{ row.warehouse_code }}</t-form-item>
+      </div>
+      <div class="form-item-box">
+        <t-form-item label="销售订单"> {{ row.so_no }}</t-form-item>
+        <t-form-item label="工艺路线版本"> {{ row.routingName }}</t-form-item>
+      </div>
+      <div class="form-item-box">
+        <t-form-item label="备注"> {{ row.memo }}</t-form-item>
+      </div>
+    </t-card>
     <t-row justify="space-between">
       <t-col>
         <t-space>
           <t-space direction="horizontal">
-            <t-tabs v-for="(item, index) in tabModuleList" :key="index" v-model="selectModule">
+            <t-tabs v-for="(item, index) in tabModuleList" :key="index" v-model="selectModule" @change="onChangeTab">
               <t-tab-panel :value="item.moduleCode" :label="item.moduleName"> </t-tab-panel>
             </t-tabs>
           </t-space>
@@ -49,58 +55,46 @@
     </t-row>
     <!-- table表格 -->
     <footer class="detailed-work-center">
-      <div class="table-work-header">
-        <!-- 表格 -->
+      <div v-if="selectModule == 'MOBOM'" class="table-work-header">
         <tm-table
-          ref="tableRef"
-          v-model:pagination="pageUI"
+          ref="tableBomRef"
           row-key="name"
-          :table-column="columns"
-          :table-data="workData"
-          :total="total"
-          :loading="loading"
-          :selected-row-keys="selectedRowKeys"
-          @select-change="rehandleSelectChange"
-          @refresh="fetchData"
+          :table-column="columnsBom"
+          :table-data="moBomData"
+          :loading="loadingBom"
+          :show-pagination="false"
+          @refresh="fetchTableBom"
         >
-          <!-- <template #expandedRow="{ row }">
-            <div>{{ row }}</div>
-          </template> -->
-          <template #sequence>
-            <div>1</div>
-          </template>
-          <template #Work-center-number>
-            <div>
-              <t-icon name="chevron-right"></t-icon>
-              <t-link theme="primary" underline> 0752-A01-WCO1 </t-link>
-            </div>
-          </template>
-          <template #parentWorkCenter>
-            <div>11</div>
-          </template>
-          <template #associated>
-            <t-link theme="primary" underline>PE30332705-2 </t-link>
-          </template>
-          <!-- </t-table> -->
         </tm-table>
-        <span class="table-btn"> <t-button theme="default" @click="onHandleCancellation">取消</t-button></span>
+      </div>
+
+      <div v-if="selectModule == 'MOLOG'" class="table-work-header">
+        <tm-table
+          ref="tableLogRef"
+          row-key="name"
+          :table-column="columnsLog"
+          :table-data="moLogData"
+          :loading="loadingLog"
+          :show-pagination="false"
+          @refresh="fetchTableLog"
+        >
+        </tm-table>
       </div>
     </footer>
+    <div class="popup-mo-foot-btn">
+      <t-button theme="default" @click="onHandleCancellation">取消</t-button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import _ from 'lodash';
 import { PrimaryTableCol, TableRowData } from 'tdesign-vue-next';
-import { onMounted, ref } from 'vue';
+import { nextTick, ref, watch } from 'vue';
 
-// import { api as apicontrol } from '@/api/control';
-// import { api as apimain } from '@/api/main';
+import { api as apicontrol } from '@/api/control';
 import TmTable from '@/components/tm-table/index.vue';
 import { useLoading } from '@/hooks/modules/loading';
-import { usePage } from '@/hooks/modules/page';
-
-// import TmSelectBusiness from '../../../../components/tm-select-business/index.vue';
 // 子修改传值
 const props = defineProps({
   // 组件展示类型
@@ -109,158 +103,176 @@ const props = defineProps({
     default: null,
   },
 });
-const { pageUI } = usePage();
-const { loading, setLoading } = useLoading();
-const total = ref(10);
+const { loading: loadingBom, setLoading: setLoadingBom } = useLoading();
+const { loading: loadingLog, setLoading: setLoadingLog } = useLoading();
+
 const Emit = defineEmits(['addedShow']);
 const currentrow = ref({}); // 当前行工单信息
-const selectedRowKeys = ref([]); // 用于存储选中行的数组
 // 页签
 const tabModuleList = ref([
   { moduleCode: 'MOBOM', moduleName: '工单BOM' },
   { moduleCode: 'MOLOG', moduleName: '作业日志' },
 ]);
-const selectModule = ref('MOBOM');
-const fetchData = async () => {
-  setLoading(true);
-  setTimeout(() => {
-    workData.value = _.cloneDeep(workData.value);
-    setLoading(false);
-  }, 500);
+const selectModule = ref('MOLOG');
+// 工单BOM信息
+const moBomData = ref([]);
+// 工单日志信息
+const moLogData = ref([]);
+// 查询工单BOM
+const fetchTableBom = async () => {
+  try {
+    setLoadingBom(true);
+    const row = currentrow.value as any;
+    if (row) {
+      const { id } = row;
+      const res = (await apicontrol.moBom.getMoBomListByMoCode({
+        moCode: id,
+      })) as any;
+      moBomData.value = res.list;
+    }
+  } catch (e) {
+    console.log('cus', e);
+  } finally {
+    setLoadingBom(false);
+  }
 };
-const columns: PrimaryTableCol<TableRowData>[] = [
+
+// 查询工单LOG
+const fetchTableLog = async () => {
+  try {
+    setLoadingLog(true);
+    const row = currentrow.value as any;
+    if (row) {
+      const { id } = row;
+      const res = (await apicontrol.moLog.getMoLogListByMoCode({
+        moCode: id,
+      })) as any;
+      moLogData.value = res.list;
+    }
+  } catch (e) {
+    console.log('cus', e);
+  } finally {
+    setLoadingLog(false);
+  }
+};
+const columnsBom: PrimaryTableCol<TableRowData>[] = [
   {
-    colKey: 'select',
-    type: 'multiple',
-  },
-  {
-    colKey: 'sequence',
-    title: '顺序号',
+    colKey: 'serial-number',
+    title: '序号',
     align: 'center',
   },
   {
-    colKey: 'Work-center-number',
-    title: '工作中心编号',
+    colKey: 'mitemCode',
+    title: '组件编码',
     align: 'center',
   },
   {
-    colKey: 'name',
-    title: '名称',
+    colKey: 'mitemDesc',
+    title: '组件名称',
     align: 'center',
   },
   {
-    colKey: 'types',
-    title: '类型',
+    colKey: 'bomVersion',
+    title: 'BOM版本',
+    align: 'center',
+    width: 120,
+  },
+  {
+    colKey: 'numeratorQty',
+    title: '用量分子',
     align: 'center',
   },
   {
-    colKey: 'location',
-    title: '地点',
+    colKey: 'denomainatorQty',
+    title: '用量分母',
     align: 'center',
   },
   {
-    colKey: 'associated',
-    title: '关联设备',
+    colKey: 'uom',
+    title: '单位',
     align: 'center',
   },
   {
-    colKey: 'head',
-    title: '负责人',
+    colKey: 'moRequestQty',
+    title: '需求数量',
+    align: 'center',
+  },
+  {
+    colKey: 'moRealQty',
+    title: '实发数量',
     align: 'center',
   },
 ];
-const workData = ref([
+const columnsLog: PrimaryTableCol<TableRowData>[] = [
   {
-    name: ['你好2'],
-    types: '11',
-    Workshop: 'Aag',
-    location: '唱不上',
-    associated: '-',
-    head: '李四',
+    colKey: 'serial-number',
+    title: '序号',
+    align: 'center',
   },
   {
-    name: ['你好1'],
-    types: '11',
-    Workshop: 'Aag',
-    location: '唱不上',
-    associated: '-',
-    head: '李四',
+    colKey: 'timeCreate',
+    title: '作业时间',
+    align: 'center',
   },
   {
-    name: ['你好3'],
-    types: '11',
-    Workshop: 'Aag',
-    location: '唱不上',
-    associated: '-',
-    head: '李四',
+    colKey: 'logContent',
+    title: '作业内容',
+    align: 'center',
   },
-]);
-const formData = ref({
-  workNumber: '', // 工作中心编号
-  workName: '', // 工作中心名称
-  location: '', // 地点
-  workShop: '', // 所属车间
-  head: '', // 负责人
-  parent: '', // 父级
-});
-// const typeData = ref([
-//   {
-//     id: 1,
-//     name: '工作区',
-//     show: true,
-//   },
-//   {
-//     id: 2,
-//     name: '生产线',
-//     show: false,
-//   },
-//   {
-//     id: 3,
-//     name: '工段',
-//     show: false,
-//   },
-//   {
-//     id: 4,
-//     name: '设备',
-//     show: false,
-//   },
-// ]);
-
-// 高亮事件
-// const onHandleCur = (id: number) => {
-//   typeData.value.forEach((item) => {
-//     if (item.id === id) {
-//       item.show = true;
-//     } else {
-//       item.show = false;
-//     }
-//   });
-// };
-// checked事件
-const rehandleSelectChange = (value: any, ctx: any) => {
-  selectedRowKeys.value = value;
-  console.log('value:', value, '1', ctx);
-};
+  {
+    colKey: 'displayName',
+    title: '作业人员',
+    align: 'center',
+  },
+  {
+    colKey: 'operationType',
+    title: '说明',
+    align: 'center',
+  },
+];
 // 取消
 const onHandleCancellation = () => {
   Emit('addedShow', false);
 };
 
-// 初始化远程数据
-onMounted(() => {
-  if (props.row) {
-    currentrow.value = props.row;
+// 切换模块
+const onChangeTab = () => {
+  if (selectModule.value === 'MOBOM') {
+    fetchTableBom();
   }
-});
-
-const submit = () => {
-  console.log(1);
+  if (selectModule.value === 'MOLOG') {
+    fetchTableLog();
+  }
 };
+watch(
+  () => props.row,
+  (value) => {
+    nextTick(() => {
+      currentrow.value = value;
+      selectModule.value = 'MOBOM';
+      fetchTableBom();
+      fetchTableLog();
+    });
+  },
+  { deep: true },
+);
 </script>
 
 <style lang="less" scoped>
+.t-card {
+  border: 1px solid var(--td-border-level-2-color) !important;
+  margin-bottom: 24px;
+}
+
 .detailed-box {
-  padding: var(--td-comp-paddingTB-xxl) var(--td-comp-paddingLR-xxl);
+  padding: var(--td-comp-paddingTB-xl) var(--td-comp-paddingLR-xl);
+
+  .popup-mo-foot-btn {
+    display: block;
+    height: 30px;
+    margin-top: 20px;
+    text-align: right;
+  }
 }
 
 .type-box {
@@ -291,29 +303,24 @@ const submit = () => {
 }
 // 表单盒子边距
 .form-item-box {
-  margin: 10px 0;
+  margin: 0;
+
+  .t-form__item {
+    display: inline-block;
+    width: 30%;
+    margin: 0;
+  }
+
+  /deep/ .t-form__label {
+    display: inline-block !important;
+  }
+
+  /deep/ .t-form__controls {
+    display: inline-block !important;
+  }
 }
 
 .form-checkbox {
   margin: 10px 100px;
-}
-// 子工作中心
-.detailed-work-center {
-  display: flex;
-
-  .work-header {
-    margin: 20px;
-  }
-
-  .table-work-header {
-    flex: 1;
-    // 表格按钮
-    .table-btn {
-      float: right;
-      display: block;
-      height: 30px;
-      margin: 10px;
-    }
-  }
 }
 </style>
