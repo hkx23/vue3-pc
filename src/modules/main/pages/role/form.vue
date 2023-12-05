eecec4e8cf2692b1ce7c096548176db93348fb3d
Wayne <361425807@qq.com>
2023-12-05 08:58:54 +0800

    fix: 角色参考

---
+++ src/modules/main/pages/role/form.vue
@@ -0,0 +1,126 @@
+<template>
+  <t-form
+    ref="formRef"
+    :rules="FORM_RULES"
+    :data="formData"
+    :show-cancel="true"
+    :show-error-message="true"
+    :label-width="135"
+  >
+    <t-form-item :label="t('role.roleCode')" name="roleCode">
+      <t-input v-model="formData.roleCode" clearable :disabled="isFormEditing" />
+    </t-form-item>
+
+    <t-form-item :label="t('role.roleName')" name="roleName">
+      <t-input v-model="formData.roleName" clearable />
+    </t-form-item>
+    <t-form-item :label="t('role.eId')" name="eid">
+      <tm-select-business
+        v-model="formData.eid"
+        type="enterprise"
+        :disabled="!isAdmin"
+        :show-title="false"
+      ></tm-select-business>
+    </t-form-item>
+    <t-form-item :label="t('role.org')" name="oid">
+      <tm-select-business
+        v-model="formData.oid"
+        type="plant"
+        :parent-id="formData.eid"
+        :disabled="!isAdmin"
+        :show-title="false"
+      ></tm-select-business>
+    </t-form-item>
+    <t-form-item :label="t('role.roleDesc')" name="roleDesc">
+      <t-textarea v-model="formData.roleDesc" clearable />
+    </t-form-item>
+  </t-form>
+</template>
+<script lang="ts">
+export default {
+  name: 'RoleForm',
+};
+</script>
+<script setup lang="ts">
+import { FormInstanceFunctions, MessagePlugin } from 'tdesign-vue-next';
+import { reactive, Ref, ref } from 'vue';
+
+import { api, Role } from '@/api/main';
+import { useUserStore } from '@/store';
+
+import { FormRef } from './constants';
+import { useLang } from './lang';
+
+const { t } = useLang();
+const user = useUserStore();
+const formRef: Ref<FormInstanceFunctions> = ref(null);
+const FORM_RULES = {
+  roleCode: [{ required: true, message: t('common.placeholder.input', [t('role.roleCode')]) }],
+  roleName: [{ required: true, message: t('common.placeholder.input', [t('role.roleName')]) }],
+  eid: [{ required: true, message: t('common.placeholder.select', [t('role.eId')]) }],
+  oid: [{ required: true, message: t('common.placeholder.select', [t('role.org')]) }],
+};
+
+const formData: Role = reactive({
+  roleCode: '',
+  roleName: '',
+  roleDesc: '',
+  oid: '',
+  eid: '',
+});
+
+const submit = async () => {
+  return new Promise((resolve, reject) => {
+    formRef.value.validate().then((result) => {
+      if (result !== true) {
+        MessagePlugin.warning(Object.values(result)[0][0].message);
+        reject();
+        return;
+      }
+      if (isFormEditing) {
+        api.role.edit(formData).then(() => {
+          MessagePlugin.success(t('common.message.saveSuccess'));
+          resolve(formData);
+        });
+        return;
+      }
+      api.role.add(formData).then(() => {
+        MessagePlugin.success(t('common.message.addSuccess'));
+        resolve(formData);
+      });
+    });
+  });
+};
+let isAdmin = false;
+let isFormEditing = false;
+const reset = (isEdit: boolean, data?: Role) => {
+  isAdmin = false;
+  formRef.value.reset({ type: 'empty' });
+  formData.id = '';
+  formData.oid = '';
+  formData.eid = '';
+  // 将id小写后比较
+
+  if (user.userInfo.id.toLowerCase() === 'administrator') {
+    isAdmin = true;
+  } else if (!isEdit) {
+    formData.eid = fw.getEnterpriseId();
+    formData.oid = fw.getOrgId();
+  }
+
+  isFormEditing = isEdit;
+  if (data) {
+    if (isEdit) {
+      Object.assign(formData, data);
+    }
+  }
+};
+
+defineExpose({
+  form: formRef,
+  submit,
+  reset,
+} as FormRef);
+</script>
+<style lang="less" scoped></style>
+`
