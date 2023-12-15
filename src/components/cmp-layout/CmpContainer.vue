d67865141a2ee6ba86d7afc5c664a8d8e2ae2796
Jesse <Jcay60@163.com>
2023-12-15 12:55:41 +0800

    feat: 布局组件支持多子组件占满高度

--- src/components/cmp-layout/CmpContainer.vue
+++ src/components/cmp-layout/CmpContainer.vue
@@ -9,22 +9,48 @@ export default {
 };
 </script>
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue';
+import { computed, nextTick, onMounted, ref, watch } from 'vue';
 
 const props = defineProps<{
   full?: boolean;
-  flexIndex?: number;
+  fullSubIndex?: number[];
 }>();
 const classAttrs = computed(() => {
   return `cmp-container ${props.full ? 'cmp-container-full' : ''}`;
 });
 
 const container = ref(null);
-onMounted(() => {
+
+watch(
+  () => props.fullSubIndex,
+  (val) => {
+    if (!val) return;
+    nextTick(setFull);
+  },
+);
+
+watch(
+  () => props.full,
+  (val) => {
+    if (!val) return;
+    nextTick(setFull);
+  },
+);
+
+const setFull = () => {
   const elements = container.value.$el.querySelectorAll(':scope > .t-space-item') as HTMLInputElement[];
-  if (elements.length > 0) {
-    elements[props.flexIndex || elements.length - 1].style.flex = '1';
+  if (elements.length > 0 && props.fullSubIndex) {
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i];
+      elements[i].style.flex = props.fullSubIndex.indexOf(i) >= 0 ? '1' : 'none';
+    }
+  } else {
+    elements[elements.length - 1].style.flex = '1';
   }
+};
+
+onMounted(() => {
+  setFull();
 });
 </script>
 <style lang="less" scoped>
@@ -37,9 +63,5 @@ onMounted(() => {
   height: 100%;
   flex: 1;
   overflow: hidden;
-
-  // :deep(> .t-space-item) {
-  //   height: 100%;
-  // }
 }
 </style>
