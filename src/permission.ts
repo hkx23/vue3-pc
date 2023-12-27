6947e74b2faaeeb7acb54745ca7909a92ccd37fc
Ben <6489@qq.com>
2023-12-27 10:42:50 +0800

    feat: 新增常用菜单

--- src/permission.ts
+++ src/permission.ts
@@ -6,7 +6,7 @@ import { RouteRecordRaw } from 'vue-router';
 
 import { CustomError } from '@/assets/libs/web-core';
 import router from '@/router';
-import { getPermissionStore, useUserStore } from '@/store';
+import { getPermissionStore, getUserTabsHistoryStore, useUserStore } from '@/store';
 import { PAGE_NOT_FOUND_ROUTE } from '@/utils/route/constant';
 
 NProgress.configure({ showSpinner: false });
@@ -82,5 +82,14 @@ router.afterEach((to) => {
     userStore.logout();
     permissionStore.restoreRoutes();
   }
+  const userTabsHistoryStore = getUserTabsHistoryStore();
+
+  userTabsHistoryStore.appendTabHistoryList({
+    path: to.path,
+    title: to.name as string,
+    name: to.name,
+    isAlive: true,
+    meta: to.meta,
+  });
   NProgress.done();
 });
