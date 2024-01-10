9b64bc15520283cda103c8e53badf61e1dae9b55
Wayne <361425807@qq.com>
2024-01-10 21:13:47 +0800

    fix: 导入组件

--- src/modules/demo/pages/import/index.vue
+++ src/modules/demo/pages/import/index.vue
@@ -1,5 +1,10 @@
 <template>
   <cmp-container :full="true">
-    <bcmp-import-button />
+    <bcmp-import-button theme="primary" type="mitemCategory" @close="fetchTable"></bcmp-import-button>
   </cmp-container>
 </template>
+<script setup lang="tsx">
+const fetchTable = () => {
+  console.log('导入成功');
+};
+</script>
