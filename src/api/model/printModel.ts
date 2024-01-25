fb5a2ce7161daa50073baca5d775df282d2a9a76
Jesse <Jcay60@163.com>
2024-01-25 22:18:44 +0800

    feat: 添加打印接口

---
+++ src/api/model/printModel.ts
@@ -0,0 +1,10 @@
+export interface stimulsoftTemplate {
+  /** 模板内容 */
+  templateBody: string;
+  /** 模板中数据节点名称 (默认DataBase) */
+  dataName?: string;
+  /** 模板需要的数据 */
+  data?: object;
+  /** 打印机名称（windows为空则取默认，linux必填） */
+  printerName?: string;
+}
