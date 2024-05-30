f344945fc38a91d54d705d317bbe418a1c58111b
Symon <522529675@qq.com>
2024-05-30 15:48:23 +0800

    feat: restful界面初始化

---
+++ src/modules/main/pages/restfulTool/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <cmp-container :full="true">
+    <cmp-row>
+      <cmp-card :span="3"></cmp-card>
+      <cmp-card :span="9"></cmp-card>
+    </cmp-row>
+  </cmp-container>
+</template>
+
+<script setup lang="ts"></script>
